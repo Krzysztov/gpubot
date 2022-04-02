a2d42f8db7342bab7dfb5028c4e1aaa428405f2d
Krzysztov <krzysiek.radej.ken@gmail.com>
2022-04-02 20:57:43 +0200

    app use

--- src/api.js
+++ src/api.js
@@ -2,6 +2,10 @@ import fs from 'fs';
 import got from 'got';
 import jsdom from 'jsdom';
 import TeleBot from 'telebot';
+import express from 'express';
+
+const app = express();
+const port = 3000;
 
 const bot  = new TeleBot('5106718787:AAHtj2WJ88m0Ep2_FmVYdfJGEPzL4ox-RUk');
 
@@ -14,21 +18,30 @@ const url = "https://www.krsystem.pl/index.php?szukaj=radeon+6600xt&kat=0&go=szu
 
 let stareCeny = [];
 
-setInterval(() => {
-    got(url).then(response => {
-        const dom = new JSDOM(response.body);
-        const ceny = dom.window.document.body.querySelectorAll('div .cena a');
-        
-            ceny.forEach(cena => {
-                if(!stareCeny.includes(cena.textContent.substring(1,6))) 
-                {stareCeny.push(cena.textContent.substring(1,6))
-                 bot.sendMessage(2075117382, `Pojawiły się zmiany w Twoim wyszukiwaniu ${url}`);
-                 console.log("powiadomienie o nowej cenie")}
-                
-            });
+function getRequest () {
  
-    }).catch(err => {
-        console.log(err);
-    });
-}, 5000)
+        got(url).then(response => {
+            const dom = new JSDOM(response.body);
+            const ceny = dom.window.document.body.querySelectorAll('div .cena a');
+            
+                ceny.forEach(cena => {
+                    if(!stareCeny.includes(cena.textContent.substring(1,6))) 
+                    {stareCeny.push(cena.textContent.substring(1,6))
+                     bot.sendMessage(2075117382, `Pojawiły się zmiany w Twoim wyszukiwaniu ${url}`);
+                     console.log("powiadomienie o nowej cenie")}
+                    
+                });
+     
+        }).catch(err => {
+            console.log(err);
+        });
+}
+
+getRequest();
+
+setInterval(getRequest, 5000);
+
+app.listen(port, () => {
+    console.log('server is running');
+})
 
